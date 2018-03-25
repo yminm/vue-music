0b08ff836963096193ac0eb6930b2009c0d470ad
minmin <942532185@qq.com>
2018-03-25 21:43:06 +0800

    项目骨架搭建

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import FastClick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+import store from './store'
 
+
+FastClick.attach(document.body)
+
+Vue.use(VueLazyload,{
+  loading: require('common/image/default')
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
